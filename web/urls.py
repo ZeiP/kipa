c51e64f564c4b83a49f34c497102d1af5a7b59fc
Ilmari Marttila <ilmari.marttila@gmail.com>
2024-11-21 12:49:27 +0200

    Pass callable views to url() and declare urlpatterns as list
     django.conf.urls.patterns is deprecated
     and will be removed in Django 2.0.

--- web/urls.py
+++ web/urls.py
@@ -1,24 +1,25 @@
 from __future__ import absolute_import
-from django.conf.urls import include, patterns
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.static import serve
+
+from tupa.views import raportti_500
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    "",
-    (r"^kipa/", include("tupa.urls")),
-    (r"^admin/", include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r"^kipa/", include("tupa.urls")),
+    url(r"^admin/", include(admin.site.urls)),
+]
 
 if settings.SERVE_MEDIA:
-    urlpatterns += patterns(
-        "",
-        (
+    urlpatterns += [
+        url(
             r"^kipamedia/(?P<path>.*)$",
-            "django.views.static.serve",
+            serve,
             {"document_root": settings.STATIC_DOC_ROOT},
-        ),
-    )
+        )
+    ]
 
-handler500 = "tupa.views.raportti_500"
+handler500 = raportti_500
