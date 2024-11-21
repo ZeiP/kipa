579e00baad4c353a3612dbbe0c2a38871bbd0f2d
Ilmari Marttila <ilmari.marttila@gmail.com>
2024-11-21 12:49:27 +0200

    Pass callable views to url() and declare urlpatterns as list
     django.conf.urls.patterns is deprecated
     and will be removed in Django 2.0.

--- web/tupa/urls.py
+++ web/tupa/urls.py
@@ -2,82 +2,83 @@
 #    Copyright (C) 2010  Espoon Partiotuki ry. ept@partio.fi
 
 from __future__ import absolute_import
-from django.conf.urls import patterns
+from django.conf.urls import url
 from django.conf import settings
+from django.views.static import serve
+
+from .views import *
 
 tal = r"(?P<talletettu>(talletettu)?)/?$"
 
-urlpatterns = patterns(
-    "tupa.views",
-    (r"^apua/", "apua"),
-    (r"^$", "etusivu"),
-    (r"^post_txt/(?P<parametrit>[^/]+)/$", "post_txt"),
-    (r"^(?P<kisa_nimi>[^/]+)/tallenna/$", "tallennaKisa"),
-    (r"^login/$", "loginSivu"),
-    (r"^logout/$", "logoutSivu"),
-    (r"^lisaaKisa/$", "korvaaKisa"),
-    (r"^(?P<kisa_nimi>[^/]+)/$", "kisa"),
-    (r"^uusiKisa/maarita/$", "maaritaKisa"),
-    (r"^(?P<kisa_nimi>[^/]+)/korvaa/$", "korvaaKisa"),
-    (r"^(?P<kisa_nimi>[^/]+)/poista/$", "poistaKisa"),
-    (r"^(?P<kisa_nimi>[^/]+)/maarita/" + tal, "maaritaKisa"),
-    (r"^(?P<kisa_nimi>[^/]+)/maarita/tehtava/$", "maaritaValitseTehtava"),
-    (
+urlpatterns = [
+    url(r"^apua/", apua),
+    url(r"^$", etusivu),
+    url(r"^post_txt/(?P<parametrit>[^/]+)/$", post_txt),
+    url(r"^(?P<kisa_nimi>[^/]+)/tallenna/$", tallennaKisa),
+    url(r"^login/$", loginSivu),
+    url(r"^logout/$", logoutSivu),
+    url(r"^lisaaKisa/$", korvaaKisa),
+    url(r"^(?P<kisa_nimi>[^/]+)/$", kisa),
+    url(r"^uusiKisa/maarita/$", maaritaKisa),
+    url(r"^(?P<kisa_nimi>[^/]+)/korvaa/$", korvaaKisa),
+    url(r"^(?P<kisa_nimi>[^/]+)/poista/$", poistaKisa),
+    url(r"^(?P<kisa_nimi>[^/]+)/maarita/" + tal, maaritaKisa),
+    url(r"^(?P<kisa_nimi>[^/]+)/maarita/tehtava/$", maaritaValitseTehtava),
+    url(
         r"^(?P<kisa_nimi>[^/]+)/maarita/tehtava/uusi/sarja/(?P<sarja_id>\d+)/$",
-        "maaritaTehtava",
+        maaritaTehtava,
     ),
-    (
+    url(
         r"^(?P<kisa_nimi>[^/]+)/maarita/tehtava/(?P<tehtava_id>\d+)/" + tal,
-        "maaritaTehtava",
+        maaritaTehtava,
     ),
-    (
+    url(
         r"^(?P<kisa_nimi>[^/]+)/maarita/vaiheet/(?P<tehtava_id>\d+)/(?P<vartio_id>\d*)/?",
-        "tehtavanVaiheet",
+        tehtavanVaiheet,
     ),
-    (r"^(?P<kisa_nimi>[^/]+)/maarita/vartiot/" + tal, "maaritaVartiot"),
-    (
+    url(r"^(?P<kisa_nimi>[^/]+)/maarita/vartiot/" + tal, maaritaVartiot),
+    url(
         r"^(?P<kisa_nimi>[^/]+)/maarita/tehtava/kopioi/sarjaan/(?P<sarja_id>\d+)/$",
-        "kopioiTehtavia",
+        kopioiTehtavia,
     ),
-    (r"^(?P<kisa_nimi>[^/]+)/maarita/testitulos/" + tal, "testiTulos"),
-    (
+    url(r"^(?P<kisa_nimi>[^/]+)/maarita/testitulos/" + tal, testiTulos),
+    url(
         r"^(?P<kisa_nimi>[^/]+)/luo/sarja/(?P<sarja_id>\d+)/testitulokset/$",
-        "luoTestiTulokset",
+        luoTestiTulokset,
     ),
-    (r"^(?P<kisa_nimi>[^/]+)/maarita/tuomarineuvos/" + tal, "tuomarineuvos"),
-    (r"^(?P<kisa_nimi>[^/]+)/syota/(?P<tarkistus>(tarkistus/)?)$", "syotaKisa"),
-    (
+    url(r"^(?P<kisa_nimi>[^/]+)/maarita/tuomarineuvos/" + tal, tuomarineuvos),
+    url(r"^(?P<kisa_nimi>[^/]+)/syota/(?P<tarkistus>(tarkistus/)?)$", syotaKisa),
+    url(
         r"^(?P<kisa_nimi>[^/]+)/syota/(?P<tarkistus>(tarkistus/)?)tehtava/(?P<tehtava_id>\d+)/"
         + tal,
-        "syotaTehtava",
+        syotaTehtava,
     ),
-    (r"^(?P<kisa_nimi>[^/]+)/tulosta/normaali/$", "tulosta"),
-    (
+    url(r"^(?P<kisa_nimi>[^/]+)/tulosta/normaali/$", tulosta),
+    url(
         r"^(?P<kisa_nimi>[^/]+)/tulosta/normaali/sarja/(?P<sarja_id>\d+)/$",
-        "tulostaSarja",
+        tulostaSarja,
     ),
-    (r"^(?P<kisa_nimi>[^/]+)/tulosta/tilanne/$", "laskennanTilanne"),
-    (r"^(?P<kisa_nimi>[^/]+)/tulosta/heijasta/sarja/(?P<sarja_id>\d+)/$", "heijasta"),
-    (r"^(?P<kisa_nimi>[^/]+)/tulosta/heijasta/$", "heijasta"),
-    (
+    url(r"^(?P<kisa_nimi>[^/]+)/tulosta/tilanne/$", laskennanTilanne),
+    url(r"^(?P<kisa_nimi>[^/]+)/tulosta/heijasta/sarja/(?P<sarja_id>\d+)/$", heijasta),
+    url(r"^(?P<kisa_nimi>[^/]+)/tulosta/heijasta/$", heijasta),
+    url(
         r"^(?P<kisa_nimi>[^/]+)/tulosta/tuloste/sarja/(?P<sarja_id>\d+)/$",
-        "tulostaSarjaHTML",
+        tulostaSarjaHTML,
     ),
-    (r"^(?P<kisa_nimi>[^/]+)/tulosta/tuloste/$", "tulosta"),
-    (
+    url(r"^(?P<kisa_nimi>[^/]+)/tulosta/tuloste/$", tulosta),
+    url(
         r"^(?P<kisa_nimi>[^/]+)/tulosta/csv/sarja/(?P<sarja_id>\d+)/$",
-        "sarjanTuloksetCSV",
+        sarjanTuloksetCSV,
     ),
-    (r"^(?P<kisa_nimi>[^/]+)/tulosta/csv/$", "tulosta"),
-    (r"^(?P<kisa_nimi>[^/]+)/tulosta/piirit/$", "piirit"),
-)
+    url(r"^(?P<kisa_nimi>[^/]+)/tulosta/csv/$", tulosta),
+    url(r"^(?P<kisa_nimi>[^/]+)/tulosta/piirit/$", piirit),
+]
 
 if settings.DEBUG:
-    urlpatterns += patterns(
-        "",
-        (
+    urlpatterns += [
+        url(
             r"^kipamedia/(?P<path>.*)$",
-            "django.views.static.serve",
+            serve,
             {"document_root": settings.STATIC_DOC_ROOT},
         ),
-    )
+    ]
